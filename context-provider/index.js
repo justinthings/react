99e3d90f7d06fe40c07078e46ae2dd16bfbb68c6
thingsjust <justindavidross@gmail.com>
2020-02-08 17:10:28 -0500

    Demo Context Provider and Consumer
    Demo Context Provider and Consumer

---
+++ context-provider/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render } from "react-dom";
+import styled from "styled-components";
+const StylesContext = React.createContext();
+const StylesProvider = StylesContext.Provider;
+const StylesConsumer = StylesContext.Consumer;
+const stylesDefault = { color: "red" };
+const stylesFromUser = { color: "blue" };
+
+const HeaderWrapper = styled.div`
+  color: ${(styles) => styles.color};
+`;
+
+const Header = props => (
+  <StylesConsumer>
+    {styles => (
+      // this is where things that are shared, are available to child components of Layout:
+      <HeaderWrapper {...styles}>{props.children}</HeaderWrapper>
+    )}
+  </StylesConsumer>
+);
+
+const Layout = props => (
+  <StylesProvider value={{ ...stylesDefault, ...props.styles, ...props }}>
+    {props.children}
+  </StylesProvider>
+);
+
+render(
+  <Layout styles={stylesFromUser}>
+    <Header>A Header</Header>
+  </Layout>,
+  document.getElementById("root")
+);
